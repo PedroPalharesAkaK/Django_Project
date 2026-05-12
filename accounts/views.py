6edbe9040c90389f04af94c435b248410adae446
Pedro Palhares <pedro.palhares58@gmail.com>
2026-05-12 12:05:59 -0300

    novo forms de accounts e mudancas em views para usar o SignUpForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,15 @@
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.forms import UserCreationForm
+# Remova: from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm  # Importe o seu novo formulário
 from django.shortcuts import render, redirect
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST) # Use o SignUpForm aqui
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm() # E aqui também
     return render(request, 'signup.html', {'form': form})
