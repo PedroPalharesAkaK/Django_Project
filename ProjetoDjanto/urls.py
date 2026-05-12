219af5444f075b25b6940456a8f4beef3f6393c8
Pedro Palhares <pedro.palhares58@gmail.com>
2026-05-12 14:35:52 -0300

    mudancas em base para nao usar Jquery, mudanas em signup para block content, adicao de uma url de login

--- ProjetoDjanto/urls.py
+++ ProjetoDjanto/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('signup/', accounts_views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'), #temporario
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
