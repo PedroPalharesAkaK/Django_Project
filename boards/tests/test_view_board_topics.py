f5c5a7bc72d61cbe7791afb5a1b5a9ea32e77c00
Pedro Palhares <pedro.palhares58@gmail.com>
2026-05-18 21:50:56 -0300

    muduancas nos testes para GBCV

--- boards/tests/test_view_board_topics.py
+++ boards/tests/test_view_board_topics.py
@@ -1,45 +1,49 @@
 from django.urls import reverse, resolve
 from django.test import TestCase
-from ..views import home, board_topics, new_topic
+# CORREÇÃO 1: Removidos os nomes antigos 'home' e 'board_topics' que não existem mais no views.py
+from ..views import new_topic, TopicListView
 from ..models import Board, Topic, Post
 from django.contrib.auth.models import User
 from ..forms import NewTopicForm
 
 class BoardTopicsTests(TestCase):
     def setUp(self):
-        # Cria um board para os testes
-        Board.objects.create(name='Django', description='Django board.')
+        # CORREÇÃO 2: Guardando o objeto criado em self.board para os métodos usarem dinamicamente
+        self.board = Board.objects.create(name='Django', description='Django board.')
 
     def test_board_topics_view_success_status_code(self):
-        url = reverse('board_topics', kwargs={'pk': 1})
+        url = reverse('board_topics', kwargs={'pk': self.board.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_board_topics_view_not_found_status_code(self):
-        url = reverse('board_topics', kwargs={'pk': 99})
+        # Passa um ID que sabidamente não existe (somando 1 ao ID atual)
+        url = reverse('board_topics', kwargs={'pk': self.board.pk + 1})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
     def test_board_topics_url_resolves_board_topics_view(self):
-        view = resolve('/boards/1/')
-        self.assertEqual(view.func, board_topics)
+        # Agora o self.board.pk vai funcionar perfeitamente!
+        view = resolve(f'/boards/{self.board.pk}/')
+        # CORREÇÃO DJANGO 6: Compara com a classe usando .view_class
+        self.assertEqual(view.func.view_class, TopicListView)
 
     def test_board_topics_view_contains_link_back_to_homepage(self):
-        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
         response = self.client.get(board_topics_url)
         homepage_url = reverse('home')
-        # Usando f-string em vez de .format()
         self.assertContains(response, f'href="{homepage_url}"')
+
     def test_board_topics_view_contains_navigation_links(self):
-        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
         homepage_url = reverse('home')
-        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+        new_topic_url = reverse('new_topic', kwargs={'pk': self.board.pk})
 
         response = self.client.get(board_topics_url)
 
-     # Verifica o link de volta para a Home
+        # Verifica o link de volta para a Home
         self.assertContains(response, f'href="{homepage_url}"')
-    # Verifica o link para criar um novo tópico
+        # Verifica o link para criar um novo tópico
         self.assertContains(response, f'href="{new_topic_url}"')
 
 
