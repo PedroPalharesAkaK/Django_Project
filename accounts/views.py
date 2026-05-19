72e4a8cbef2937bed6d4c3f1e8407e51d9839556
Pedro Palhares <pedro.palhares58@gmail.com>
2026-05-19 12:33:14 -0300

    adicao de settings/account

--- accounts/views.py
+++ accounts/views.py
@@ -13,3 +13,24 @@ def signup(request):
     else:
         form = SignUpForm() # E aqui também
     return render(request, 'signup.html', {'form': form})
+
+# accounts/views.py
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import UpdateView
+
+# ... suas views anteriores de signup ...
+
+@method_decorator(login_required, name='dispatch')
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ('first_name', 'last_name', 'email', )
+    template_name = 'my_account.html'
+    success_url = reverse_lazy('my_account')
+
+    def get_object(self):
+        # Retorna sempre o usuário logado atualmente
+        return self.request.user
