fc1c08b8c31bb715ae7f900c1c6e03ab4a4380dc
Pedro Palhares <pedro.palhares58@gmail.com>
2026-05-20 15:54:18 -0300

    adicionado decouple para esconder secret key

--- ProjetoDjanto/settings.py
+++ ProjetoDjanto/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import sys
+from decouple import config, Csv
 
 # Se o comando executado no terminal contiver a palavra 'test', o Django usa um hasher super rápido
 if 'test' in sys.argv or 'test_coverage' in sys.argv:
@@ -27,13 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qzzfj8hr$$&7uxxu6c+h8nd1fe=b1p)yib@%q&8w%%8)6_3$vt'
+#django-insecure-qzzfj8hr$$&7uxxu6c+h8nd1fe=b1p)yib@%q&8w%%8)6_3$vt
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
