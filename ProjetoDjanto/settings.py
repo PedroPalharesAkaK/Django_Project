a1e9e7d208c68bb9811ddef01de9637710523a42
Pedro Palhares <pedro.palhares58@gmail.com>
2026-06-03 15:39:15 -0300

    obrigado runserver a rodar whitenoise

--- ProjetoDjanto/settings.py
+++ ProjetoDjanto/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
