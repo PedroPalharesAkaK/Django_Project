be92dda2fe20670976f4d7a19d5815c650db5730
Pedro Palhares <pedro.palhares58@gmail.com>
2026-06-03 15:29:17 -0300

    Configura whitenoise para arquivos estaticos em prod

--- ProjetoDjanto/settings.py
+++ ProjetoDjanto/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +130,7 @@ USE_TZ = True #muda todos horarios para utc
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/' 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     str(BASE_DIR / 'static'),  # O str() é OBRIGATÓRIO aqui para o Windows, esta barra concatena os caminhos enre base dir e static
