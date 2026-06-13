44cec2fbb1ea1ff635da016d0d89f4ad4da9213c
Pedro Palhares <pedro.palhares58@gmail.com>
2026-06-12 23:13:14 -0300

    almost nothing

--- importar_professores.py
+++ importar_professores.py
@@ -44,7 +44,7 @@ def popular_banco_dados(): #[cite: 2]
             professor, foi_criado = Professor.objects.get_or_create( #[cite: 2]
                 nome=nome, #[cite: 2]
                 defaults={ #[cite: 2]
-                    'descricao': 'Faculdade de arquitetura e urbanismo', 
+                    'descricao': 'Faculdade de Arquitetura e Urbanismo', 
                     'visualizacoes': 0, #[cite: 2]
                     'universidade': usp,
                     'instituto': fau
