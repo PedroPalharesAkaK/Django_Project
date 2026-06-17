28442ad211ffc5857b9f92639ef326bb7c85fb6c
Pedro Palhares <pedro.palhares58@gmail.com>
2026-06-16 21:18:25 -0300

    professor IRI

--- importar_professores.py
+++ importar_professores.py
@@ -10,7 +10,7 @@ from boards.models import Professor, Instituto, Universidade
 
 def popular_banco_dados(): #[cite: 2]
     # Atualizado para o novo arquivo
-    caminho_arquivo = 'Nome professores ib.txt' 
+    caminho_arquivo = 'Nome professores iri.txt' 
     
     # 2. Preparamos as chaves estrangeiras ANTES do loop começar
     usp, _ = Universidade.objects.get_or_create(
@@ -18,9 +18,9 @@ def popular_banco_dados(): #[cite: 2]
         defaults={"nome": "Universidade de São Paulo"}
     )
     
-    ib, _ = Instituto.objects.get_or_create(
+    iri, _ = Instituto.objects.get_or_create(
         sigla="IB",
-        defaults={"nome": "Instituto de Biociências", "universidade": usp}
+        defaults={"nome": "Instituto de Relações Internacionais", "universidade": usp}
     )
 
     # Abre o arquivo garantindo a leitura correta de acentos (utf-8)[cite: 2]
@@ -30,7 +30,7 @@ def popular_banco_dados(): #[cite: 2]
     cadastrados = 0 #[cite: 2]
     ignorados = 0 #[cite: 2]
 
-    print("Iniciando a importação de docentes do ib...\n") 
+    print("Iniciando a importação de docentes do iri...\n") 
 
     for linha in linhas: #[cite: 2]
         # Remove quebras de linha (\n) e espaços nas pontas[cite: 2]
@@ -44,10 +44,10 @@ def popular_banco_dados(): #[cite: 2]
             professor, foi_criado = Professor.objects.get_or_create( #[cite: 2]
                 nome=nome, #[cite: 2]
                 defaults={ #[cite: 2]
-                    'descricao': 'Instituto de Biociências', 
+                    'descricao': 'Instituto de Relações Internacionais', 
                     'visualizacoes': 0, #[cite: 2]
                     'universidade': usp,
-                    'instituto': ib
+                    'instituto': iri
                 }
             )
 
